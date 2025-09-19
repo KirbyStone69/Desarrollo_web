610a17744a75e6517a3e4527d0274600470930e6
KirbyStone69 <2430206@upv.edu.mx>
2025-09-19 12:10:11 -0600

    d

--- vectoresdinamicos.cpp
+++ vectoresdinamicos.cpp
@@ -3,12 +3,10 @@
 int main(){
     int *ptr_a, *ptr_oux;
     ptr_a = nullptr; ptr_oux = nullptr;
-
     //ptr_a = new int; // apunta a un entero
     ptr_a = new int[1]; //apunta a un vector de un solo elemento
     int tam = 1;
     ptr_a[0] = 5;
-
     for (int i = 0; i < 10; i++){
         int x;
         std::cin >> x;
